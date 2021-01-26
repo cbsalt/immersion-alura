71e180dd7db2a652b80c4e8cb73fe81ab6802608
CHARLES SAL <cbsal.tec@gmail.com>
2021-01-26 14:31:17 -0300

    feat: linter, routes and state

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json'
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
 * {
@@ -21,17 +24,21 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -17,21 +21,39 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Alura Quiz</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>React Quiz</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste numquam dicta blanditiis error ea veniam sit necessitatibus. Sit, nam provident totam voluptatum, voluptates aspernatur maxime corporis velit perferendis adipisci earum?</p>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={(event) => setName(event.target.value)}
+                placeholder="Insira seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Content>
-            <h1>JS Quiz</h1>
-
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste numquam dicta blanditiis error ea veniam sit necessitatibus. Sit, nam provident totam voluptatum, voluptates aspernatur maxime corporis velit perferendis adipisci earum?</p>
           </Widget.Content>
         </Widget>
@@ -39,5 +61,5 @@ export default function Home() {
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/cbsalt" />
     </QuizBackground>
-  )
+  );
 }
